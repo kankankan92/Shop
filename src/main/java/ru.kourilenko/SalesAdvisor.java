9367efb70c46c298d115197894445dcdcd2fb535
nastya <kankankan92@mail.ru>
2015-06-02 23:39:58 +0400

    Условие цикла. Цикл foreach.

--- src/main/java/ru.kourilenko/SalesAdvisor.java
+++ src/main/java/ru.kourilenko/SalesAdvisor.java
@@ -3,6 +3,14 @@ package ru.kourilenko;
 public class SalesAdvisor {
 
     public void move(Hall hall, Storage storage){
+        for(int i = 0; i<hall.rails.length; i++){
+
+        }
+        for (Rail rail : hall.rails){}
+
+
+
+
 
 
     }
